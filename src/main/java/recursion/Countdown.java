3507ae38b2ba8ad4bb405da1e71cbe3b72371329
StasVaryn <mr.varyn@mail.ru>
2019-09-12 23:51:42 +0300

    recursion

---
+++ src/main/java/recursion/Countdown.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class Countdown {
+
+    private static void countdown(int i) {
+        System.out.println(i);
+
+        if (i <= 0) {
+            return;
+        } else {
+            countdown(i - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        countdown(5);
+    }
+}
